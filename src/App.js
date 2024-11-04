1a5cea5d8843c0860d0770af2428cbd79a730b3f
karmpatels <124847567+karmpatels@users.noreply.github.com>
2024-11-03 21:33:11 -0500

    Lab 7

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1> Welcome to Fullstack Development - I </h1>
+        <h3> React JS Programming Week09 Lab exercise </h3>
+        <h4> 101410114 </h4>
+        <h5> Karm Patel </h5>
+        <h6> George Brown College, Toronto </h6>
+      </header>
+    </div>
+  );
+}
+
+export default App;
